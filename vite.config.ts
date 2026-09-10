442fee3c25be64ebb7d49306e4a991a1bc57bdf7
Lez <nip5:lez@nostr.hu>
2026-09-10 11:23:31 +0200

    Set fixed port, no clash with other locally developed apps.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {hmr: false}
+  server: {
+    hmr: false, 
+    headers: { 'Cache-Control': 'no-store' },
+    port: 1234,
+    strictPort: true,  // fail if taken
+  }
 })
